0aabdcb519616ed5b955e87f4f1f5a7baa438f10
Mikhail Slinko <slinkom@gmail.com>
2021-08-20 01:05:28 +0300

    add: unique constraint for Account

---
+++ TodoApiService/Migrations/20210819220236_UniqueIndexForAccounts.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TodoApiService.Migrations
+{
+    public partial class UniqueIndexForAccounts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Accounts",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Accounts",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_Email",
+                table: "Accounts",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_Phone",
+                table: "Accounts",
+                column: "Phone",
+                unique: true,
+                filter: "[Phone] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_Email",
+                table: "Accounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_Phone",
+                table: "Accounts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Accounts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
--- TodoApiService/Models/TodoApiApplicationContext.cs
+++ TodoApiService/Models/TodoApiApplicationContext.cs
@@ -7,5 +7,12 @@ namespace TodoApiService.Models
         public TodoApiApplicationContext(DbContextOptions<TodoApiApplicationContext> options) : base(options){}
         public DbSet<Account> Accounts { get; set; }
         public DbSet<RefreshToken> RefreshTokens { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+                .HasIndex(a => a.Email).IsUnique();
+            modelBuilder.Entity<Account>()
+                .HasIndex(a => a.Phone).IsUnique();
+        }
     }
 }
