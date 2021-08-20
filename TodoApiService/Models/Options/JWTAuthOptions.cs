bdbe1c81433cd0964b4c8cf5d49eeabb0a9b9df5
Mikhail Slinko <slinkom@gmail.com>
2021-08-20 03:29:52 +0300

    add: parameters pattern for config auth

---
+++ TodoApiService/Models/Options/JWTAuthOptions.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TodoApiService.Models.Options
+{
+    public class JWTAuthOptions
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string Secret { get; set; }
+        public int TokenLifeTime { get; set; }
+        public TimeSpan ClockSkew { get; set; }
+        public SymmetricSecurityKey GetSymmetricSecurityKey() =>
+            new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Secret));
+    }
+}
