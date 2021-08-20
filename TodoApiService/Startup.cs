9b9a1f7be7fb516b62236cd8a2e0295c5a151dc1
Mikhail Slinko <slinkom@gmail.com>
2021-08-20 03:33:27 +0300

    add: jwt autification

--- TodoApiService/Startup.cs
+++ TodoApiService/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TodoApiService.Models;
+using TodoApiService.Models.Options;
 
 namespace TodoApiService
 {
@@ -27,6 +29,36 @@ namespace TodoApiService
             {
                 options.UseSqlServer(Configuration.GetConnectionString("TodoApiApplicationDb"));
             });
+
+            services.Configure<JWTAuthOptions>(Configuration.GetSection("JWTAuth"));
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(jwt =>
+                {
+                    jwt.SaveToken = true;
+                    JWTAuthOptions jwtAuthOptions = Configuration.Get<JWTAuthOptions>();
+                    jwt.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = jwtAuthOptions.Issuer,
+
+                        ValidateAudience = true,
+                        ValidAudience = jwtAuthOptions.Audience,
+
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = jwtAuthOptions.GetSymmetricSecurityKey(),
+            
+                        ValidateLifetime = true,
+                        
+                        ClockSkew = jwtAuthOptions.ClockSkew
+                    };
+                });
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -37,6 +69,10 @@ namespace TodoApiService
             }
 
             app.UseRouting();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
