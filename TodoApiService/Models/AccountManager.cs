0e33588e81eb4814262d6201901a7bf01cd33b16
Mikhail Slinko <slinkom@gmail.com>
2021-08-20 04:43:05 +0300

    add: add implementation generation access token

--- TodoApiService/Models/AccountManager.cs
+++ TodoApiService/Models/AccountManager.cs
@@ -1,12 +1,41 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using TodoApiService.Models.DTO.Authentication;
+using TodoApiService.Models.Options;
 
 namespace TodoApiService.Models
 {
     public class AccountManager : IAccountManager
     {
+        private readonly JWTAuthOptions _jwtAuthOptions;
+        private readonly TodoApiApplicationContext _appDbContext;
+        public AccountManager(IOptions<JWTAuthOptions> jwtAuthOptions, TodoApiApplicationContext appDbContext)
+        {
+            _jwtAuthOptions = jwtAuthOptions.Value;
+            _appDbContext = appDbContext;
+        }
         public TokenResult GenerateJWTTokens(Account account)
         {
-            throw new System.NotImplementedException();
+            TokenResult tokenResult = new TokenResult();
+            JwtSecurityTokenHandler jwtTokenHandler = new JwtSecurityTokenHandler();
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new []
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, account.Id.ToString()),
+                    new Claim(JwtRegisteredClaimNames.Iss, _jwtAuthOptions.Issuer),
+                    new Claim(JwtRegisteredClaimNames.Aud, _jwtAuthOptions.Audience),
+                    new Claim(JwtRegisteredClaimNames.Email, account.Email?.ToString()),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                }), 
+                Expires = DateTime.UtcNow.AddSeconds(_jwtAuthOptions.TokenLifeTime),
+                SigningCredentials = new SigningCredentials(_jwtAuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256)
+            };
+            SecurityToken token = jwtTokenHandler.CreateToken(tokenDescriptor);
+            tokenResult.AccessToken = jwtTokenHandler.WriteToken(token);
         }
 
         public TokenResult LoginAccount(LoginAccountCredentials loginCredentials)
